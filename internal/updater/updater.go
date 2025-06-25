a7b3bff4f34ceaa6420ff02bbd915a7db31d219f
Sindre Rødseth Hansen <sindre.rodseth.hansen@nav.no>
2025-06-25 09:04:45 +0200

    fix: update daily vulnerability summary refresh cron expression to 9:15 AM CEST

--- internal/updater/updater.go
+++ internal/updater/updater.go
@@ -16,8 +16,8 @@ import (
 
 const (
 	FetchVulnerabilityDataForImagesDefaultLimit = 10
-	MarkUntrackedCronInterval                   = "*/20 * * * *"         // every 20 minutes
-	RefreshVulnerabilitySummaryCronDailyView    = "30 5 * * *:0 7 * * *" // every day at 7:30 AM CEST
+	MarkUntrackedCronInterval                   = "*/20 * * * *" // every 20 minutes
+	RefreshVulnerabilitySummaryCronDailyView    = "15 7 * * *"   // every day at 9:15 AM CEST
 	MarkAsUntrackedAge                          = 30 * time.Minute
 )
 
