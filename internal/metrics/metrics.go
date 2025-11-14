69e63aa9adbda151c5dea008d7869e31ba4db686
Youssef Bel Mekki <38552193+ybelMekk@users.noreply.github.com>
2025-11-14 09:11:59 +0100

    debug: add robust logging for OTLP exporter initialization
    * add DNS lookup
    * add collector to fqdn

--- internal/metrics/metrics.go
+++ internal/metrics/metrics.go
@@ -3,6 +3,8 @@ package metrics
 import (
 	"context"
 	"fmt"
+	"net"
+	"net/url"
 	"os"
 
 	"github.com/jackc/pgx/v5/pgxpool"
@@ -22,7 +24,25 @@ import (
 	semconv "go.opentelemetry.io/otel/semconv/v1.37.0"
 )
 
-func NewMeterProvider(ctx context.Context, extraCollectors ...promClient.Collector) (*sdkmetric.MeterProvider, *sdktrace.TracerProvider, promClient.Gatherer, error) {
+type loggingExporter struct {
+	next sdktrace.SpanExporter
+	log  logrus.FieldLogger
+}
+
+func (l *loggingExporter) ExportSpans(ctx context.Context, spans []sdktrace.ReadOnlySpan) error {
+	err := l.next.ExportSpans(ctx, spans)
+	if err != nil {
+		l.log.Errorf("OTLP EXPORT FAILED (%d spans): %v", len(spans), err)
+	}
+	return err
+}
+
+func (l *loggingExporter) Shutdown(ctx context.Context) error {
+	return l.next.Shutdown(ctx)
+}
+
+func NewMeterProvider(ctx context.Context, log logrus.FieldLogger, extraCollectors ...promClient.Collector) (*sdkmetric.MeterProvider, *sdktrace.TracerProvider, promClient.Gatherer, error) {
+
 	res, err := newResource()
 	if err != nil {
 		return nil, nil, nil, fmt.Errorf("creating resource: %w", err)
@@ -41,9 +61,7 @@ func NewMeterProvider(ctx context.Context, extraCollectors ...promClient.Collect
 		}
 	}
 
-	metricExporter, err := otelprom.New(
-		otelprom.WithRegisterer(reg),
-	)
+	metricExporter, err := otelprom.New(otelprom.WithRegisterer(reg))
 	if err != nil {
 		return nil, nil, nil, fmt.Errorf("creating prometheus exporter: %w", err)
 	}
@@ -54,10 +72,12 @@ func NewMeterProvider(ctx context.Context, extraCollectors ...promClient.Collect
 	)
 	otel.SetMeterProvider(meterProvider)
 
-	var tp *sdktrace.TracerProvider
 	primary := os.Getenv("OTEL_EXPORTER_OTLP_ENDPOINT")
 	secondary := os.Getenv("OTEL_EXPORTER_OTLP_SECONDARY")
 
+	log.Infof("OTEL tracing: primary='%s' secondary='%s'", primary, secondary)
+
+	var tp *sdktrace.TracerProvider
 	if primary != "" {
 		var opts []sdktrace.TracerProviderOption
 
@@ -69,40 +89,62 @@ func NewMeterProvider(ctx context.Context, extraCollectors ...promClient.Collect
 			),
 		))
 
-		// Primary exporter
-		exp1, err := otlptrace.New(ctx,
-			otlptracegrpc.NewClient(
-				otlptracegrpc.WithEndpointURL(primary),
-				otlptracegrpc.WithInsecure(),
-			),
-		)
+		exp1, err := newLoggedExporter(ctx, primary, log.WithField("otel_exporter", "primary"))
 		if err != nil {
-			return nil, nil, nil, fmt.Errorf("primary exporter: %w", err)
+			log.Errorf("FAILED initializing primary OTLP exporter: %v", err)
+			return nil, nil, nil, err
 		}
 		opts = append(opts, sdktrace.WithBatcher(exp1))
 
-		// Optional: Secondary exporter
 		if secondary != "" {
-			exp2, err := otlptrace.New(ctx,
-				otlptracegrpc.NewClient(
-					otlptracegrpc.WithEndpointURL(secondary),
-					otlptracegrpc.WithInsecure(),
-				),
-			)
+			exp2, err := newLoggedExporter(ctx, secondary, log.WithField("otel_exporter", "secondary"))
 			if err != nil {
-				return nil, nil, nil, fmt.Errorf("secondary exporter: %w", err)
+				log.Errorf("FAILED initializing secondary OTLP exporter: %v", err)
+				return nil, nil, nil, err
 			}
 			opts = append(opts, sdktrace.WithBatcher(exp2))
 		}
 
-		// Build tracer provider with multiple processors
 		tp = sdktrace.NewTracerProvider(opts...)
 		otel.SetTracerProvider(tp)
 	}
 
 	otel.SetTextMapPropagator(propagation.TraceContext{})
-
 	return meterProvider, tp, reg, nil
+}
+
+func newLoggedExporter(ctx context.Context, endpoint string, log logrus.FieldLogger) (sdktrace.SpanExporter, error) {
+
+	log.Infof("Initializing OTLP exporter for endpoint: %s", endpoint)
+
+	u, err := url.Parse(endpoint)
+	if err != nil {
+		log.Errorf("OTLP endpoint parse error: %v", err)
+	} else {
+		host := u.Hostname()
+		if host != "" {
+			ips, lookupErr := net.LookupHost(host)
+			if lookupErr != nil {
+				log.Errorf("DNS lookup FAILED for '%s': %v", host, lookupErr)
+			} else {
+				log.Infof("DNS lookup OK for '%s': %v", host, ips)
+			}
+		}
+	}
+
+	exp, err := otlptrace.New(ctx,
+		otlptracegrpc.NewClient(
+			otlptracegrpc.WithEndpointURL(endpoint),
+			otlptracegrpc.WithInsecure(),
+		),
+	)
+	if err != nil {
+		log.Errorf("Failed creating OTLP exporter for '%s': %v", endpoint, err)
+		return nil, err
+	}
+
+	log.Infof("OTLP exporter connected to %s", endpoint)
+	return &loggingExporter{next: exp, log: log}, nil
 }
 
 func newResource() (*resource.Resource, error) {
