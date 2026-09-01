a124d2631c88e19a6a9addbaf1f7618319ae4f84
Youssef Bel Mekki <38552193+ybelMekk@users.noreply.github.com>
2026-09-01 10:30:37 +0200

    fix(updater): correct daily refresh time for vulnerability summary cron job

--- internal/updater/updater.go
+++ internal/updater/updater.go
@@ -26,7 +26,7 @@ const (
 	FetchVulnerabilityDataForImagesDefaultLimit        = 10
 	MarkUntrackedCronInterval                          = "*/20 * * * *" // every 20 minutes
 	MarkUnusedCronInterval                             = "*/30 * * * *" // every 30 minutes
-	RefreshVulnerabilitySummaryCronDailyView           = "30 4 * * *"   // every day at 6:30 AM CEST
+	RefreshVulnerabilitySummaryCronDailyView           = "30 4 * * *"   // every day at 4:30 AM CEST
 	RefreshWorkloadVulnerabilityLifetimesCronDailyView = "0 5 * * *"    // every day at 5:00 AM CEST (30 min later)
 	SyncKevCronInterval                                = "0 6 * * *"    // every day at 8:00 AM CEST
 	SyncOsvCronInterval                                = "0 7 * * *"    // every day at 9:00 AM CEST
