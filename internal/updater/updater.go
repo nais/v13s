5c638177e8af471ef1f699344636dee8f96d2bf7
Youssef Bel Mekki <38552193+ybelMekk@users.noreply.github.com>
2026-09-11 10:50:11 +0200

    feat(updater): add integration test for batch CVE priority updates

--- internal/updater/updater.go
+++ internal/updater/updater.go
@@ -529,7 +529,7 @@ func (u *Updater) BatchUpdateVulnerabilityData(ctx context.Context, images []*Im
 			cveIDs = append(cveIDs, c.CveID)
 		}
 
-updated, err := u.querier.UpdateCvePriorityForCves(ctx, cveIDs)
+		updated, err := u.querier.UpdateCvePriorityForCves(ctx, cveIDs)
 		if err != nil {
 			return fmt.Errorf("updating cve priority after upserting CVEs: %w", err)
 		}
